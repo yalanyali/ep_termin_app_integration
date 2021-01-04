ddb53ec94a9b835890c48a508ae13cccd7f6ff6d
Yigitcan Alanyali <y.alanyali@gmail.com>
2021-01-04 21:49:58 +0100

    Update index.js
    foo

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 const fetch = require('node-fetch')
 
-const settings = require('ep_etherpad-lite/node/utils/Settings')
+const express = require('../../src/node_modules/express')
+const eejs = require('ep_etherpad-lite/node/eejs')
 const authorManager = require('ep_etherpad-lite/node/db/AuthorManager')
+
+const API_URL = process.env.TERMIN_APP_API_URL
+
+exports.expressConfigure = function (hook_name, args, cb) {
+
+  args.app.use(express.json())
+  args.app.use(express.urlencoded({ extended: true }))
+
+  args.app.post('/login', function (req, res) {
+    console.warn('POST REQUEST', req.body)
+    res.redirect(req.query.redirect || '/')
+  })
+
+  cb()
+}
+
+exports.preAuthorize = (hook_name, ctx, cb) => {
+  if (ctx.req.method === 'POST') {
+    console.warn('preAuth for ', ctx.req)
+    if (ctx.req.path.startsWith('/login')) return cb([true])
+  }
+  if (ctx.req.path.startsWith('/static')) return cb([true])
+  console.warn('FAILED PREAUTH', ctx.req.is('application/x-www-form-urlencoded'))
+  return cb([]);
+}
+
+exports.authenticate = async (hook_name, ctx, cb) => {
+
+  console.warn('ep_termin_integration.authenticate', ctx.req.path)
+
+  if (!ctx.req.is('application/x-www-form-urlencoded')) {
+    console.warn('ep_termin_integration.authenticate: Failed authentication no auth form data')
+    return cb([false])
+  }
+
+  const { email, password } = ctx.req.body
+
+  console.warn('HEADERS: ', email, password)
+
+  const response = await fetch(API_URL, {
+    method: 'post',
+    body: JSON.stringify({
+      email,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+
+  const userData = await response.json()
+
+  if (response.ok) {
+    ctx.req.session.user = {
+      username: userData.displayName,
+      is_admin: false
+    }
+    return cb([true])
+  } else {
+    return cb([false])
+  }
+
+}
+
+exports.authnFailure = function (hook_name, context, cb) {
+  console.warn('ep_termin_integration.authFailure')
+
+  const renderArgs = {
+    style: eejs.require('ep_termin_integration/templates/style.ejs', {}),
+    invalidCredentials: !!context.req.session.invalidCredentials,
+    redirect: context.req.query.redirect || context.req.path || '',
+    user: context.req.session ? (context.req.session.user || null) : null
+  }
+
+  context.res.send(eejs.require('ep_termin_integration/templates/login.ejs', renderArgs))
+
+  cb([true])
+}
 
 exports.handleMessage = async (hook_name, ctx, cb) => {
   if (ctx.message.type === 'CLIENT_READY') {
     const token = ctx.message.token
     if (!token) {
-      console.debug('ep_termin_app_integration.handleMessage: Token missing from CLIENT_READY message')
+      console.warn('ep_termin_integration.handleMessage: Token missing from CLIENT_READY message')
       return
     }
     const session = ctx.client.client.request.session
@@ -15,32 +92,4 @@ exports.handleMessage = async (hook_name, ctx, cb) => {
     authorManager.setAuthorName(authorId, session.user.username)
   }
   return cb([ctx.message])
-}
-
-exports.authenticate = async (hook_name, ctx, cb) => {
-  console.debug('ep_termin_app_integration.authenticate', ctx.req.path)
-
-  if (ctx.username && ctx.password) {
-    const response = await fetch(settings.ep_termin_app_integration.api, {
-      method: 'post',
-      body: JSON.stringify({
-        email: ctx.username,
-        password: ctx.password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    const userData = await response.json()
-
-    if (response.ok) {
-      ctx.req.session.user = {
-        username: userData.displayName,
-        is_admin: false
-      }
-      return cb([true])
-    } else {
-      return cb([false])
-    }
-  } else {
-    return cb([false])
-  }
 }
